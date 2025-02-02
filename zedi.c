b67522a330a74a19f34ecc7648d6400c12da4e1d
david.zeleniak <david.zeleniak@e-o.solutions>
2025-02-01 22:46:45 -0800

    render tabs and enable tab motion

--- zedi.c
+++ zedi.c
@@ -15,6 +15,7 @@
 
 /*** defines ***/
 #define ZEDI_VERSION "0.0.1"
+#define ZEDI_TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
@@ -33,11 +34,14 @@ enum editorKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -169,6 +173,44 @@ int getWindowSize(int *rows, int *cols){
 }
 
 /***  row operations ***/
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+
+  for(int i = 0; i < cx; i++) { 
+    if (row->chars[i] == '\t') {
+      rx += (ZEDI_TAB_STOP-1) - (rx % ZEDI_TAB_STOP);
+    }
+    rx++;
+  }
+
+  return rx;
+}
+void editorUpdateRow(erow *row) {
+  int tabs = 0; 
+
+  for(int i = 0; i < row->size; i++){
+    if (row->chars[i] == '\t') tabs++; 
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + (tabs * ZEDI_TAB_STOP) + 1);
+
+  int idx = 0;
+  for(int i = 0; i < row->size; i++) {
+    if(row->chars[i] == '\t') {
+      row->render[idx++] = ' ';
+      while(idx % ZEDI_TAB_STOP != 0) {
+        row->render[idx++] = ' ';
+      }
+    } else {
+      row->render[idx++] = row->chars[i];
+    }
+  }
+
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows+1));
   int at = E.numrows;
@@ -176,6 +218,11 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len+1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+  
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -221,17 +268,22 @@ void abFree(struct abuf *ab){
 
 /*** output ***/
 void editorScroll() {
+  E.rx = E.cx;
+  if(E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+
   if(E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if(E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   } 
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -257,10 +309,10 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else { 
-      int len = E.row[filerow].size-E.coloff;
+      int len = E.row[filerow].rsize-E.coloff;
       if (len <  0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -281,7 +333,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff)+1, (E.cx-E.coloff)+1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff)+1, (E.rx-E.coloff)+1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -367,6 +419,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
