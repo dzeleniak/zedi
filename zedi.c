bb665489df91b1b8c7f856157e77b30c26c06168
david.zeleniak <david.zeleniak@e-o.solutions>
2025-02-01 22:58:42 -0800

    END key functionality

--- zedi.c
+++ zedi.c
@@ -393,12 +393,19 @@ void editorProcessKeypress() {
       break;
 
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
       break;
 
     case PAGE_UP: 
     case PAGE_DOWN:
       {
+        if(c == PAGE_UP) { 
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) { 
+          E.cy = E.rowoff + E.screenrows-1;
+        }
+
         int times = E.screenrows;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
